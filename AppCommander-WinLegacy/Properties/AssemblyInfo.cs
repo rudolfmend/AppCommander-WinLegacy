17fd07d3bc6f14af59250fe6dee6b195901987cf
rudolfmend <rudolf.mend@gmail.com>
2025-05-04 21:25:24 +0200

    Initial commit - základná štruktúra projektu

---
+++ AppCommander-WinLegacy/Properties/AssemblyInfo.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AppCommander-WinLegacy")]
+[assembly: AssemblyDescription("Automation tool for interacting with text fields and buttons in other applications. \" +\r\n    \"Allows you to calculate values, insert them into text fields, and confirm them using a button in the controlled application. \" +\r\n    \"The program can identify and manipulate user interface elements of external applications. \" +\r\n    \"Allows you to define, save, and load automation step configurations. \" +\r\n    \"Allows you to perform repeated actions (repeat in a loop). \" +\r\n    \"For automating repetitive tasks in various applications without the need for programming. \" +\r\n    \"Detection of all installed applications, including hidden system applications.\" +\r\n    \"Supports OS Windows 7, 8 and 8.1 onwards.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Rudolf Mendzezof")]
+[assembly: AssemblyProduct("AppCommander-WinLegacy")]
+[assembly: AssemblyCopyright("Copyright © Rudolf Mendzezof 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+//In order to begin building localizable applications, set
+//<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
+//inside a <PropertyGroup>.  For example, if you are using US english
+//in your source files, set the <UICulture> to en-US.  Then uncomment
+//the NeutralResourceLanguage attribute below.  Update the "en-US" in
+//the line below to match the UICulture setting in the project file.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+                                     //(used if a resource is not found in the page,
+                                     // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+                                              //(used if a resource is not found in the page,
+                                              // app, or any theme specific resource dictionaries)
+)]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
